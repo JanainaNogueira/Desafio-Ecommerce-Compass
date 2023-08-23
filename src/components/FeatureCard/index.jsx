98d9070f45b8e1346c63796b40a6dfcf33599d92
raul.bana <raul.ferreira@gmail.com>
2023-08-22 23:51:06 -0300

    fix: feature-card style

--- src/components/FeatureCard/index.jsx
+++ src/components/FeatureCard/index.jsx
@@ -1,11 +1,12 @@
 import classes from './index.module.css'
 
+// eslint-disable-next-line react/prop-types
 const FeatureCard = ({description,image,title})=>{
     return (
     <div className={classes.featureCard}>
         <div className={classes.featureText}>
-            <h1>{title}</h1>
-            <p>{description}</p>
+            <h1 className={classes.featureTitle}>{title}</h1>
+            <p className={classes.featureDescription}>{description}</p>
         </div>
         <div className={classes.featureImage}>{image}</div>
     </div>
