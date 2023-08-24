03819893b947414d09158add3dc288f46cb6fad6
JanainaNogueira <janainanogueira464@gmail.com>
2023-08-23 21:56:46 -0300

    create initial banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/header/header.jsx'
+import Banner from './components/Banner'
 
 function App() {
 
   return (
     <>
+    <Header/>
+    <Banner/>
     </>
   )
 }
