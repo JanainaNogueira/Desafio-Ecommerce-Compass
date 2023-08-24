a0afc105e25acd769e52ed3526b3720834ddafa9
raul.bana <raul.ferreira@gmail.com>
2023-08-23 23:03:07 -0300

    refactor: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
-import Header from "./components/header/header";
-import Footer from "./components/Footer";
-import BuyProduct from "./pages/BuyProduct";
+
 
 function App() {
   return (
     <>
-      <Header />
-        <BuyProduct></BuyProduct>
-      <Footer />
     </>
   );
 }
