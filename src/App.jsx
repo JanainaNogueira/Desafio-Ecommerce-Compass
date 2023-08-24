a8e8a2a875331a80123da55d2ca7767e0469d032
raul.bana <raul.ferreira@gmail.com>
2023-08-24 13:28:35 -0300

    fix: app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import ProductsContextProvider from "./context/products-context";
-import Header from './components/header/header'
-import Footer from './components/Footer/index'
+
 
 function App() {
   return (
