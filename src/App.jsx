0092bb7a9e8e139b40d7068d62a75884feae5e49
raul.bana <raul.ferreira@gmail.com>
2023-08-24 16:10:06 -0300

    feat: CartPage Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import ProductsContextProvider from "./context/products-context";
 import BuyProduct from "./pages/BuyProduct";
+import CartScreen from "./pages/CartScreen";
 
 const router = createBrowserRouter([
   { path: "/products/:productId", element: <BuyProduct></BuyProduct> },
+  { path: "/cart", element: <CartScreen></CartScreen> },
 ]);
 
 function App() {
