4af4b4456a5003d5536848291253a12a274c067f
JanainaNogueira <janainanogueira464@gmail.com>
2023-08-24 21:19:44 -0300

    feat: Create promotion product section

---
+++ src/components/SectionPromotion/index.jsx
@@ -0,0 +1,17 @@
+import ProductSale from '../ProductSale'
+import Styles from './index.module.css'
+function sectionProduct(){
+    return(
+        <section className={Styles.sectionPromotion}>
+            <div className={Styles.sectionPromotion__title}>
+            <p>Ofertas terminando em breve</p>
+            </div>
+            <div className={Styles.sectionPromotion__cards}>
+                <ProductSale/>
+                <ProductSale/>
+            </div>
+        </section>
+    )
+}
+
+export default sectionProduct
