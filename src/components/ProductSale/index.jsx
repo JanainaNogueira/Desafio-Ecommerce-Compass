168361831f9e549a04f3c84910602bef5dfc9269
JanainaNogueira <janainanogueira464@gmail.com>
2023-08-28 04:14:53 -0300

    feat: Creates a function to change the status of product marking flags

--- src/components/ProductSale/index.jsx
+++ src/components/ProductSale/index.jsx
@@ -1,15 +1,17 @@
 import Styles from './index.module.css'
-import AddCartButton from '../CartButton'
+import ButtonSale from '../ButtonSale'
 import FavoriteButton from '../FavoriteButton'
 import Star from '../Star'
 import BoxTimer from '../BoxTimer'
+import Flag from '../FlagDiscountNew'
+import { useState } from 'react'
 //import FlagDiscountNew from '../FlagDiscountNew'
 // eslint-disable-next-line react/prop-types
-const CardProduct = ({product})=>{
+const CardProduct = ({product,statusFlag})=>{
     // eslint-disable-next-line react/prop-types
     const{title,price,image,description,rating}=product||{};
     const starsArray = [0, 1, 2, 3, 4];
-    
+    const[newStatusFlag,setNewStatusFlag]=useState(false)
     return(
         <article className={Styles.card}>
             <div className={Styles.cardDescription}>
@@ -24,16 +26,20 @@ const CardProduct = ({product})=>{
                     </div>
                     <div className={Styles.promotionalPrice}>
                         <p>de {price?price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}): price}</p>
-                        <p>por {price?((price*25)/100).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}):'R$'+price}</p>
+                        <p>por {price?(price-(price*0.25)).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}):'R$'+price}</p>
                     </div>
                 </div>
                 <BoxTimer/>
                 <div className={Styles.buttons}>
                     <FavoriteButton/>
-                    <AddCartButton/>
+                    <ButtonSale product={product}/>
                 </div>
             </div>
-            <img className={Styles.imgCard} src={image}/>
+            <div className={Styles.imgCard}>
+            <Flag statusFlag={newStatusFlag}/>
+                <img className={Styles.imgCard__img} src={image}/>
+            </div>
+            
         </article>
     )
 }
