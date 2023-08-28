408f832f2b6916c3648617e072991058ca42e4d9
JanainaNogueira <janainanogueira464@gmail.com>
2023-08-28 04:16:56 -0300

    feat: Create function to add item to cart

--- src/components/CartButton/index.jsx
+++ src/components/CartButton/index.jsx
@@ -33,6 +33,7 @@ const CartButton = ( {product} ) => {
     >
       <p>Carrinho</p>
       <svg
+        className={classes.svg}
         xmlns="http://www.w3.org/2000/svg"
         width="14"
         height="14"
